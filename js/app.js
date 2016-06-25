69b1a91659554269bbb504e9dca00c806523d8cb
Rodrigo <romacas@hotmail.co.uk>
2016-06-25 19:16:17 +0100

    add new color functionality

--- js/app.js
+++ js/app.js
@@ -9,23 +9,35 @@ function changeColor() {
   var g = $("#green").val();
   var b = $("#blue").val();
   
+  //sets the spam color to the combination of all the slide colors
   $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
 }
 
 $("input[type=range]").change(changeColor);
 
 //round colored buttons are clicked
-$(".controls li").click(function() {
+$(".controls").on("click", "li", function() {
   //diselects the other buttons
   $(this).siblings().removeClass("selected");
   //selects the clicked button
   $(this).addClass("selected");
   //cache the color of the button that was clicked
   color = $(this).css( "background-color" );
+  
 });
 
 $("#revealColorSelect").click(function() {
     changeColor();
     //shows or hides the box with color sliders
     $("#colorSelect").toggle();
+  });
+  
+  $("#addNewColor").click(function() {
+    //creates a detatched jquery element
+    $newColor = $("<li></li>");
+	//sets the new element's background color to the color of the spam element
+	$newColor.css("background-color", $("#newColor").css("background-color"));
+	//adds a new color button to the controls
+	$(".controls ul").append($newColor);
+	$newColor.click();
   });
